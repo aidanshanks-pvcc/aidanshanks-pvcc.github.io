c408d3b149b141282185b093b3ba6b13a94b6324
aidanshanks-pvcc <ats17602@email.vccs.edu>
2024-10-30 19:34:30 -0400

    Add files via upload

---
+++ helloworld.py
@@ -0,0 +1,8 @@
+#Name: Aidan Shanks
+#Purpose:Hello World
+
+print("Hello World! This is my 40th something Python program")
+print("I can program in Python, HTML, and a variety of Cs, which is great.")
+print("Regrettably, HTML doesn't do math, which means we need to switch languages")
+print("Python can: \n\t Add \n\t Subtract \n\t Multiply \n\t Divide")
+print("And lots of other things")
